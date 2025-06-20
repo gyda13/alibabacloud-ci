56567a33da048ab3d524000d097d53f39e580b07
gyda <galmohaimeed@rematadvanced.sa>
2025-06-21 02:32:11 +0300

    simple api

--- src/main/java/com/example/springalibaba/Controller.java
+++ src/main/java/com/example/springalibaba/Controller.java
@@ -1,4 +1,16 @@
 package com.example.springalibaba;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
 public class Controller {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello from Alibaba Cloud!";
+    }
+
 }
