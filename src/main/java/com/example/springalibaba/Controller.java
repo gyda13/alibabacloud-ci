13a041a5af50b8070a141623e5cfe2cf4f9d65ec
gyda <gyda2100@gmail.com>
2026-04-13 11:31:21 +0300

    feat: add interactive intro game

--- src/main/java/com/example/springalibaba/Controller.java
+++ src/main/java/com/example/springalibaba/Controller.java
@@ -4,13 +4,21 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Map;
+import java.util.LinkedHashMap;
+
 @RestController
 @RequestMapping("/api")
 public class Controller {
 
-    @GetMapping("/hello")
-    public String hello() {
-        return "Hello from Alibaba Cloud!";
+    @GetMapping("/whoami")
+    public Map<String, Object> hello() {
+        Map<String, Object> res = new LinkedHashMap<>();
+        res.put("message", "Hey there! You found the API. 👋");
+        res.put("author", "Gyda");
+        res.put("stack", "Spring Boot on Alibaba Cloud ECS");
+        res.put("tip", "Head to / for a proper introduction 😎");
+        return res;
     }
 
 }
